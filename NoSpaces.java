19930e6f2813ed4208ee1407128dab0fa0e849f2
Karishma290395 <91252844+Karishma290395@users.noreply.github.com>
2021-11-19 16:33:02 +0530

    Add files via upload

---
+++ NoSpaces.java
@@ -0,0 +1,11 @@
+package test5;
+
+public class NoSpaces {
+
+	public static void main(String[] args) {
+
+	String str = "india is my country";
+	
+	System.out.println(str.replaceAll("\\s",""));}
+
+}
